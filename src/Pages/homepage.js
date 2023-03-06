a860038db8af00417c3d61bae111b090e9501e2c
sudenurmeydann <117362906+sudenurmeydann@users.noreply.github.com>
2023-03-06 19:35:38 +0300

    Add files via upload

---
+++ src/Pages/homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Img from "../Assets/sue.jpeg";
+import "./homepage.css";
+const Homepage = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <img src={Img} alt="" className="sue"/>
+    </>
+  );
+};
+export default Homepage;
---
+++ src/Pages/project.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+<h1>projects</h1>
+  );
+};
+export default Projects;
